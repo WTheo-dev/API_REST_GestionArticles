8fe427ee2986130491ba4f5cb7ab5625cc544921
Theo <wazydrag.theo@gmail.com>
2023-03-09 10:33:09 +0100

    bdd

---
+++ bdd.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : jeu. 09 mars 2023 à 09:32
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `apiarticles`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `articles`
+--
+
+DROP TABLE IF EXISTS `articles`;
+CREATE TABLE IF NOT EXISTS `articles` (
+  `Id_Articles` int NOT NULL AUTO_INCREMENT,
+  `titre` varchar(50) DEFAULT NULL,
+  `Contenu` text,
+  `date_de_publication` date DEFAULT NULL,
+  `nbLike` int DEFAULT NULL,
+  `nbDislike` int DEFAULT NULL,
+  `Id_Utilisateur` int NOT NULL,
+  PRIMARY KEY (`Id_Articles`),
+  KEY `Id_Utilisateur` (`Id_Utilisateur`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `like_dislikearticles`
+--
+
+DROP TABLE IF EXISTS `like_dislikearticles`;
+CREATE TABLE IF NOT EXISTS `like_dislikearticles` (
+  `Id_Like_DislikeArticles` int NOT NULL AUTO_INCREMENT,
+  `type` varchar(50) DEFAULT NULL,
+  `Id_Utilisateur` int NOT NULL,
+  `Id_Articles` int NOT NULL,
+  PRIMARY KEY (`Id_Like_DislikeArticles`),
+  KEY `Id_Utilisateur` (`Id_Utilisateur`),
+  KEY `Id_Articles` (`Id_Articles`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `Id_Utilisateur` int NOT NULL AUTO_INCREMENT,
+  `nomUtilisateur` varchar(50) DEFAULT NULL,
+  `motdepasse` varchar(50) DEFAULT NULL,
+  `rôle` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`Id_Utilisateur`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
