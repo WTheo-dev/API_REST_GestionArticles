e31aa92547af69ccd018bbcd9d2e1eba2d9d9d2b
Theo <wazydrag.theo@gmail.com>
2023-03-10 11:32:49 +0100

    modif bdd

--- bdd.sql
+++ bdd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 09 mars 2023 à 09:32
+-- Généré le : ven. 10 mars 2023 à 10:26
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -49,12 +49,26 @@ CREATE TABLE IF NOT EXISTS `articles` (
 DROP TABLE IF EXISTS `like_dislikearticles`;
 CREATE TABLE IF NOT EXISTS `like_dislikearticles` (
   `Id_Like_DislikeArticles` int NOT NULL AUTO_INCREMENT,
-  `type` varchar(50) DEFAULT NULL,
+  `type` tinyint(1) DEFAULT NULL,
   `Id_Utilisateur` int NOT NULL,
   `Id_Articles` int NOT NULL,
   PRIMARY KEY (`Id_Like_DislikeArticles`),
   KEY `Id_Utilisateur` (`Id_Utilisateur`),
   KEY `Id_Articles` (`Id_Articles`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rôle`
+--
+
+DROP TABLE IF EXISTS `rôle`;
+CREATE TABLE IF NOT EXISTS `rôle` (
+  `Id_Rôle` int NOT NULL AUTO_INCREMENT,
+  `id1_moderator` varchar(50) DEFAULT NULL,
+  `id2_publisher` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`Id_Rôle`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -68,8 +82,9 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `Id_Utilisateur` int NOT NULL AUTO_INCREMENT,
   `nomUtilisateur` varchar(50) DEFAULT NULL,
   `motdepasse` varchar(50) DEFAULT NULL,
-  `rôle` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`Id_Utilisateur`)
+  `Id_Rôle` int NOT NULL,
+  PRIMARY KEY (`Id_Utilisateur`),
+  KEY `Id_Rôle` (`Id_Rôle`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
 
--- bddtest.sql
+++ bddtest.sql
@@ -1,26 +1,14 @@
-INSERT INTO Utilisateur (nomUtilisateur, motdepasse, rôle)
-VALUES ('user1', 'password1', 'moderator'),
-       ('user2', 'password2', 'publisher'),
-       ('user3', 'password3', 'publisher'),
-       ('user4', 'password4', 'moderator'),
-       ('user5', 'password5', 'publisher');
+INSERT INTO Rôle (id1_moderator, id2_publisher)
+VALUES ('JohnModerator', 'JanePublisher');
+
+INSERT INTO Utilisateur (nomUtilisateur, motdepasse, Id_Rôle)
+VALUES ('JaneDoe', 'motdepasse', 1);
 
 INSERT INTO Articles (titre, Contenu, date_de_publication, nbLike, nbDislike, Id_Utilisateur)
-VALUES ('Article 1', 'Contenu de l\'article 1', '2022-01-01', 10, 2, 1),
-       ('Article 2', 'Contenu de l\'article 2', '2022-02-02', 20, 5, 1),
-       ('Article 3', 'Contenu de l\'article 3', '2022-03-03', 30, 8, 2),
-       ('Article 4', 'Contenu de l\'article 4', '2022-04-04', 40, 10, 3),
-       ('Article 5', 'Contenu de l\'article 5', '2022-05-05', 50, 15, 4);
-       
+VALUES ('Nouvelles tendances de la mode', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar tincidunt sapien, et rhoncus nunc bibendum ac. Quisque maximus ligula purus, nec ultrices ex consequat vitae. ', '2023-03-10', 10, 2, 1);
 
 INSERT INTO Like_DislikeArticles (type, Id_Utilisateur, Id_Articles)
-VALUES ('like', 1, 1),
-       ('like', 2, 1),
-       ('dislike', 3, 1),
-       ('like', 2, 2),
-       ('dislike', 3, 2),
-       ('like', 1, 3),
-       ('like', 3, 3),
-       ('dislike', 1, 4),
-       ('dislike', 2, 4),
-       ('dislike', 5, 5);
+VALUES (1, 1, 1); -- utilisateur JaneDoe a aimé l'article
+
+INSERT INTO Like_DislikeArticles (type, Id_Utilisateur, Id_Articles)
+VALUES (0, 2, 1); -- utilisateur JohnDoe a pas aimé l'article
